fea451ffab3f17bf625d9a43186d7d5cfc7a737d
Maria Pallares <mpallareslara@gmail.com>
2021-09-28 16:59:14 +0200

    FEAT: db from mongoAtlas connected

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require ('express')
-
+const connectDB = require('./config/db')
 const app = express()
+require('dotenv').config()
+
+connectDB()
 
 const PORT = process.env.PORT || 5000
 
