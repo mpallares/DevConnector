1744cb4f94a6884e8f08a8e8722cc39d034be60f
Maria Pallares <mpallareslara@gmail.com>
2021-09-30 14:15:25 +0200

    Created basic routes set up: users, auth, posts, profile

--- server/server.js
+++ server/server.js
@@ -9,6 +9,11 @@ const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => res.send('API running'))
 
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`)
 })
