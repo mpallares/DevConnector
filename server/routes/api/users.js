f9dca8bddc1b26ed47228004ee60b4e216006f5a
Maria Pallares <mpallareslara@gmail.com>
2021-12-18 20:45:20 +0100

    FEAT: jwt implemented on users route, sending back the token correctly

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const User = require('../../models/User');
 
 router.post(
@@ -26,7 +28,9 @@ router.post(
     try {
       let user = await User.findOne({ email });
       if (user) {
-        res.status(400).json({ erros: [{ msg: 'User already exists' }] });
+        return res
+          .status(400)
+          .json({ erros: [{ msg: 'User already exists' }] });
       }
 
       const avatar = gravatar.url(email, {
@@ -46,9 +50,23 @@ router.post(
 
       user.password = await bcrypt.hash(password, salt);
 
-      await user.save()
+      await user.save();
 
-      res.send('User route');
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
