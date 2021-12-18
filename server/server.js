ed9f1ab07314a479309fbb996268adad8bb3c3e7
Maria Pallares <mpallareslara@gmail.com>
2021-12-18 19:18:35 +0100

    FEAT: Added validation for name, email, and password to the POST Users route

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ connectDB()
 
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => res.send('API running'))
+app.use(express.json({extended: false}))
 
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
@@ -15,5 +15,5 @@ app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
 
 app.listen(PORT, () => {
-  console.log(`Server started on ${PORT}`)
+  console.log(`Server started on http://localhost:${PORT}`)
 })
