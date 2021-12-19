6d5774ff3bb2630fc0940284dbaa2cf7195f3260
Maria Pallares <mpallareslara@gmail.com>
2021-12-19 20:54:14 +0100

    FEAT: allow user to add experiences in api/profile/experience

--- server/controllers/profile.controller.js
+++ server/controllers/profile.controller.js
@@ -119,7 +119,36 @@ const deleteProfileAndUser = async (req, res) => {
   }
 };
 
+const updateProfileExperience = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { title, company, location, from, to, current, description } = req.body;
+
+  const newExp = {
+    title,
+    company,
+    location,
+    from,
+    to,
+    current,
+    description,
+  };
+
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    profile.experience.unshift(newExp)
+    await profile.save()
+    res.json(profile)
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+};
+
 module.exports = {
+  updateProfileExperience,
   deleteProfileAndUser,
   getCurrentUserProfile,
   createUpdateProfile,
--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {
+  updateProfileExperience,
   deleteProfileAndUser,
   getUserIdProfile,
   getAllProfiles,
@@ -10,24 +11,20 @@ const {
 const auth = require('../../middleware/auth');
 const { check } = require('express-validator');
 
-
 // @route    GET api/profile
 // @desc     Get all profiles
 // @access   Public
-router.get('/', getAllProfiles)
-
+router.get('/', getAllProfiles);
 
 // @route    GET api/profile/user/:user_id
 // @desc     Get profile by user ID
 // @access   Public
-router.get('/user/:user_id', getUserIdProfile)
-
+router.get('/user/:user_id', getUserIdProfile);
 
 // @route    GET api/profile/me
 // @desc     Get current users profile
 // @access   Private
 router.get('/me', auth, getCurrentUserProfile);
-
 
 // @route    POST api/profile
 // @desc     Create or update user profile
@@ -44,10 +41,25 @@ router.post(
   createUpdateProfile
 );
 
-
 // @route    DELETE api/profile
 // @desc     Delete profile, user & posts
 // @access   Private
-router.delete('/', auth, deleteProfileAndUser)
+router.delete('/', auth, deleteProfileAndUser);
+
+// @route    PUT api/profile/experience
+// @desc     Add profile experience
+// @access   Private
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is required').not().isEmpty(),
+      check('company', 'Company is required').not().isEmpty(),
+      check('from', 'From date is required').not().isEmpty(),
+    ],
+  ],
+  updateProfileExperience
+);
 
 module.exports = router;
