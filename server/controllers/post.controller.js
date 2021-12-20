2384f46544a89f0520274fc12ec9b6f0b8a528ca
Maria Pallares <mpallareslara@gmail.com>
2021-12-20 02:52:09 +0100

    FEAT: User is able to get all Posts

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -28,4 +28,15 @@ const createPost = async (req, res) => {
   }
 };
 
-module.exports = { createPost };
+const getAllPosts = async(req, res) => {
+  try {
+    const posts = await Post.find().sort({date: -1})
+    res.json(posts)
+    
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+}
+
+module.exports = { createPost, getAllPosts };
--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {createPost} = require('../../controllers/post.controller.js')
+const {createPost, getAllPosts} = require('../../controllers/post.controller.js')
 const auth = require('../../middleware/auth');
 const { check } = require('express-validator');
 const Post = require('../../models/Post');
@@ -13,5 +13,12 @@ router.post(
   [auth, [check('text', 'Text is required').not().isEmpty()]],
   createPost
 );
+
+
+// @route    GET api/posts
+// @desc     Get all posts
+// @access   Private
+router.get('/', auth, getAllPosts)
+
 
 module.exports = router;
