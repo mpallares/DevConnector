c95e0aeaeb347521f53a7d7bc5a5cd84cdd3e552
Maria Pallares <mpallareslara@gmail.com>
2021-12-20 02:47:17 +0100

    FEAT: Implement controller for user to add a POST

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,7 +1,17 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const {createPost} = require('../../controllers/post.controller.js')
+const auth = require('../../middleware/auth');
+const { check } = require('express-validator');
+const Post = require('../../models/Post');
 
+// @route    POST api/posts
+// @desc     Create a post
+// @access   Private
+router.post(
+  '/',
+  [auth, [check('text', 'Text is required').not().isEmpty()]],
+  createPost
+);
 
-router.get('/', (req, res) => res.send('Posts route'))
-
-module.exports = router
+module.exports = router;
